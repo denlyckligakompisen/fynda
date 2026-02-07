765357a837ea2b7fd25f21bc173bb0080b24cf23
denlyckligakompisen <frebrandberg@gmail.com>
2026-02-07 19:53:37 +0100

    changed top floor icon

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -15,7 +15,7 @@ const FilterBar = ({
                 className={`filter-icon-btn ${topFloorFilter ? 'active' : ''}`}
                 onClick={toggleTopFloor}
             >
-                <span className="material-symbols-outlined">roofing</span>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="currentColor"><path d="M120-80v-80h140v-180h180v-180h180v-180h180v-140h80v220H700v180H520v180H340v180H120Z" /></svg>
             </button>
 
             {/* Nearby */}
