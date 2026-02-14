7c61e777269eb016fdaf59a0a06f7e0f980dbdfe
denlyckligakompisen <frebrandberg@gmail.com>
2026-02-14 18:04:07 +0100

    Remove top-floor specific search URLs

--- scraper.py
+++ scraper.py
@@ -13,14 +13,10 @@ from bs4 import BeautifulSoup
 # ANTIGRAVITY CONFIG
 # =====================
 SEARCH_URLS = [
-    # Stockholm (Top Floor)
-    "https://www.booli.se/sok/till-salu?areaIds=115351,115355,2983,35,883816&floor=topFloor&maxListPrice=4000000&minLivingArea=45&upcomingSale=",
     # Stockholm (General)
     "https://www.booli.se/sok/till-salu?areaIds=115351,115355,2983,35,883816&maxListPrice=4000000&minLivingArea=45&upcomingSale=",
     # Uppsala (General)
-    "https://www.booli.se/sok/till-salu?areaIds=386699,386690,386688,870600&maxListPrice=4000000&minLivingArea=50&upcomingSale=",
-    # Uppsala (Top Floor)
-    "https://www.booli.se/sok/till-salu?areaIds=386699,386690,386688,870600&floor=topFloor&maxListPrice=4000000&minLivingArea=50&upcomingSale="
+    "https://www.booli.se/sok/till-salu?areaIds=386699,386690,386688,870600&maxListPrice=4000000&minLivingArea=50&upcomingSale="
 ]
 
 DELAY_SECONDS = float(os.getenv("CRAWL_DELAY_SECONDS", "4.5"))
