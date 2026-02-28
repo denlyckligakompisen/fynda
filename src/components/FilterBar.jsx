9918613879cfc93622cbea7026f7ebb1f479cf08
denlyckligakompisen <frebrandberg@gmail.com>
2026-02-28 17:03:37 +0100

    UI: Center filters in FilterBar

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -57,7 +57,7 @@ const FilterBar = ({
                     pb: 0.5,
                     width: '100%',
                     px: 2,
-                    justifyContent: 'flex-start',
+                    justifyContent: 'center',
                     alignItems: 'center',
                     '::-webkit-scrollbar': { display: 'none' },
                     scrollbarWidth: 'none'
@@ -103,7 +103,7 @@ const FilterBar = ({
                         pb: 0.5,
                         width: '100%',
                         px: 2,
-                        justifyContent: 'flex-start',
+                        justifyContent: 'center',
                         '::-webkit-scrollbar': { display: 'none' },
                         scrollbarWidth: 'none'
                     }}
